31e537c50e5fd8ca9547e57f6d5ad4963605c629
PetrDvoracek <pedro.dvoracek@gmail.com>
2019-09-29 22:28:24 +0200

    added basic docker stuff

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+'use strict';
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const express = require('express');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
